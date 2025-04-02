e7444a6d5d3c5433b9d2b9bc266a264ac902c171
Enzo-CIEL <enzocammarata0@gmail.com>
2025-04-02 10:09:52 +0200

    Crédit et bug réglé

--- game.c
+++ game.c
@@ -4,17 +4,16 @@
 #include "event.h"
 
 int jouer(SDL_Renderer* renderer) {
-
-    SDL_SetRenderDrawColor(renderer, 255,0,0,255);
+    SDL_SetRenderDrawColor(renderer, 255,255,255,120);
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
+    
 	//charger image et personnage. 
 
     int continuer = 1; //a utiliser pour savoir si on continue la boucle du jeu ou si on arrête. 
     SDL_Event events;
     
     while(continuer){ //coeur du jeu ici, les actions seront repété pour faire le déplacement des différentes images, ...
-        
         //gérer les différentes actions. 
         
         
--- main.c
+++ main.c
@@ -9,7 +9,8 @@
 
 //ligne de compilation gcc main.c file.c game.c event.c charactere.c -o mario -lSDL2main -lSDL2_image -lSDL2
 
-void init(SDL_Window *window, SDL_Renderer *renderer, SDL_Texture *texture, int w, int h);
+void init(SDL_Window **window, SDL_Renderer **renderer, SDL_Texture **texture, int w, int h);
+void end_game(SDL_Window **window, SDL_Renderer **renderer, SDL_Texture **texture);
 
 int main(int argc, char *argv[])
 {
@@ -18,35 +19,30 @@ int main(int argc, char *argv[])
 	SDL_Texture *img_menu = NULL; // Texture menu
 	SDL_Event event;
 	bool done = false;
-	init(menu, renderer_menu, img_menu, LARGEUR_FENETRE, HAUTEUR_FENETRE);
+	init(&menu, &renderer_menu, &img_menu, LARGEUR_FENETRE, HAUTEUR_FENETRE);
 	while((!done) && SDL_WaitEvent(&event)){
 		switch(event.type){
 			case SDL_QUIT:
 				done = true;
-				SDL_DestroyTexture(img_menu);
-				SDL_DestroyRenderer(renderer_menu);
-				SDL_DestroyWindow(menu);
-				SDL_Quit();
 				break;
 			case SDL_KEYDOWN:
 				switch(event.key.keysym.sym){
 					case SDLK_1:
 					case SDLK_KP_1:
 						SDL_Log("1 Pressé");
+						SDL_DestroyTexture(img_menu);
 						jouer(renderer_menu);
 						break;
 					case SDLK_2:
 					case SDLK_KP_2:
 						SDL_Log("2 Pressé");
 						SDL_RenderClear(renderer_menu);
+						SDL_Texture *texture = loadImage("./img/credit.png", renderer_menu);
+						SDL_RenderCopy(renderer_menu, texture, NULL, NULL);
 						SDL_RenderPresent(renderer_menu);
 						break;
 					case SDLK_ESCAPE:
 						done = true;
-						SDL_DestroyTexture(img_menu);
-						SDL_DestroyRenderer(renderer_menu);
-						SDL_DestroyWindow(menu);
-						SDL_Quit();
 						break;
 					default:
 						break;
@@ -55,28 +51,37 @@ int main(int argc, char *argv[])
 				break;
 		}
 	}
+	end_game(&menu, &renderer_menu, &img_menu);
+
 	return 0;
 }
 
-void init(SDL_Window *window, SDL_Renderer *renderer, SDL_Texture *texture, int w, int h){
+void init(SDL_Window **window, SDL_Renderer **renderer, SDL_Texture **texture, int w, int h){
 	if(SDL_Init(SDL_INIT_VIDEO) != 0){
 		SDL_Log("Erreur initialisation SDL: ");
 		exit(0);
 	}
-	window = SDL_CreateWindow("Menu", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w, h, SDL_WINDOW_SHOWN);
+	*window = SDL_CreateWindow("Menu", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w, h, SDL_WINDOW_SHOWN);
 	if(window == NULL){
 		SDL_Log("Erreur initialisation menu: ");
 		SDL_Quit();
 		exit(0);
 	}
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	*renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
 	if(renderer == NULL){
 		SDL_Log("Erreur loadimage: ");
-		SDL_DestroyWindow(window);
+		SDL_DestroyWindow(*window);
 		SDL_Quit();
 		exit(0);
 	}
-	texture = loadImage("./img/menu.jpg", renderer);
-	SDL_RenderCopy(renderer, texture, NULL, NULL);
-	SDL_RenderPresent(renderer);
+	*texture = loadImage("./img/menu.jpg", *renderer);
+	SDL_RenderCopy(*renderer, *texture, NULL, NULL);
+	SDL_RenderPresent(*renderer);
+}
+
+void end_game(SDL_Window **window, SDL_Renderer **renderer, SDL_Texture **texture){
+	SDL_DestroyTexture(*texture);
+	SDL_DestroyRenderer(*renderer);
+	SDL_DestroyWindow(*window);
+	SDL_Quit();
 }
